e86e77931fcffa027094bf132d38ffc1cf5117ca
Marty Buchaus <marty@dabuke.com>
2023-04-25 07:21:07 -0400

    tuesday morning adding lsp-zero

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,9 +1,15 @@
 local opts = { noremap = true, silent = true }
-
-local term_opts = { silent = true }
-
--- Shorten function name
 local keymap = vim.api.nvim_set_keymap
+local term_opts = { silent = true }
+local wk = require("which-key")
+local wkopts = {
+  mode = "n",
+  prefix = "<leader>",
+  buffer = nil,
+  silent = true,
+  noremap = true,
+  nowait = true,
+}
 
 -- Modes
 --   normal_mode = "n",
@@ -49,3 +55,240 @@ keymap("x", "K", ":move '<-2<CR>gv-gv", opts)
 keymap("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
 keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
+keymap('n', '<c-j>', '<c-w>j', opts)
+keymap('n', '<c-h>', '<c-w>h', opts)
+keymap('n', '<c-k>', '<c-w>k', opts)
+keymap('n', '<c-l>', '<c-w>l', opts)
+
+local mappings = {
+
+ -------------------------------------------------------------------------------- 
+  -- Misc -- 
+ -------------------------------------------------------------------------------- 
+  ["/"] = { "<cmd>Telescope live_grep theme=ivy<cr>", "Find Text" },
+
+ -------------------------------------------------------------------------------- 
+  -- A -- 
+ -------------------------------------------------------------------------------- 
+
+ -------------------------------------------------------------------------------- 
+  -- B -- 
+ -------------------------------------------------------------------------------- 
+
+  ["b"] = {
+    name = "+buffers",
+    D = { "<cmd>bdelete!<cr>", "Delete buffer w\\o save"},
+    W = { "<cmd>noautocmd w<cr>", "Save without formatting (noautocmd)" },
+    b = { "<cmd>Telescope buffers previewer=false<cr>", "Find" },
+    d = { "<cmd>bdelete<cr>", "Delete buffer"},
+    e = { "<cmd>BufferLinePickClose<cr>", "Pick which buffer to close" },
+    j = { "<cmd>BufferLinePick<cr>", "Jump" },
+    n = { "<cmd>BufferLineCycleNext<cr>", "Next" },
+    p = { "<cmd>BufferLineCyclePrev<cr>", "Previous" },
+  },
+
+ -------------------------------------------------------------------------------- 
+  -- E -- 
+ -------------------------------------------------------------------------------- 
+
+  -- ["e"] = {
+  --   name = "+edit",
+  --   e = { "<cmd>NvimTreeToggle<cr>", "Neotree Explorer" },
+  -- },
+
+ -------------------------------------------------------------------------------- 
+  -- F -- 
+ -------------------------------------------------------------------------------- 
+
+  ["f"] = {
+    name = "+file",
+    g  = { "<cmd>lua require('telescope.builtin').git_files()<cr>", "Telescope git_files"},
+    f  = { "<cmd>lua require('telescope.builtin').find_files()<cr>", "Find files"},
+    b  = { "<cmd>Telescope file_browser<cr>", "Find files"},
+    -- N  = { "<cmd>ene <BAR> startinseert <CR>", "New File"},
+    -- r  = { "<cmd>RnvimrToggle<CR>", "Ranger" },
+    s  = { "<cmd>up<CR>", "Save" },
+   },
+
+ -------------------------------------------------------------------------------- 
+  -- G -- 
+ -------------------------------------------------------------------------------- 
+
+  -- ["g"] = {
+  --   name = "Git",
+  --   R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
+  --   b = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
+  --   d = { "<cmd>Gitsigns diffthis HEAD<cr>", "Diff", },
+  --   g = { "<cmd>lua _LAZYGIT_TOGGLE()<CR>", "Lazygit" },
+  --   j = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
+  --   k = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
+  --   p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
+  --   r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
+  --   s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
+  --   u = { "<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>", "Undo Stage Hunk"},
+  -- },
+
+
+ -------------------------------------------------------------------------------- 
+  -- H -- 
+ -------------------------------------------------------------------------------- 
+
+  ["h"] = {
+    name = "+help",
+    -- h = { "<cmd>Telescope help_tags<CR>", "Telescope Help" },
+    -- H = { "<cmd>help<CR>", "Telescope Help" },
+    ["b"] = {
+      name = "+bindings",
+      t = { "<cmd>WhichKey<cr>", "WhichkKey Top Level"},
+    },
+  },
+
+ -------------------------------------------------------------------------------- 
+  -- I -- 
+ -------------------------------------------------------------------------------- 
+
+ -------------------------------------------------------------------------------- 
+  -- J -- 
+ -------------------------------------------------------------------------------- 
+
+ -------------------------------------------------------------------------------- 
+  -- K -- 
+ -------------------------------------------------------------------------------- 
+
+ -------------------------------------------------------------------------------- 
+  -- L -- 
+ -------------------------------------------------------------------------------- 
+  --
+  -- ["l"] = {
+  --   name = "LSP",
+  --   I = { "<cmd>LspInstallInfo<cr>", "Installer Info" },
+  --   S = { "<cmd>Telescope lsp_dynamic_workspace_symbols<cr>", "Workspace Symbols", },
+  --   a = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "Code Action" },
+  --   d = { "<cmd>Telescope diagnostics bufnr=0<cr>", "Document Diagnostics", },
+  --   f = { "<cmd>lua vim.lsp.buf.format{async=true}<cr>", "Format" },
+  --   i = { "<cmd>LspInfo<cr>", "Info" },
+  --   j = { "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", "Next Diagnostic", },
+  --   k = { "<cmd>lua vim.lsp.diagnostic.goto_prev()<cr>", "Prev Diagnostic", },
+  --   l = { "<cmd>lua vim.lsp.codelens.run()<cr>", "CodeLens Action" },
+  --   q = { "<cmd>lua vim.diagnostic.setloclist()<cr>", "Quickfix" },
+  --   r = { "<cmd>lua vim.lsp.buf.rename()<cr>", "Rename" },
+  --   s = { "<cmd>Telescope lsp_document_symbols<cr>", "Document Symbols" },
+  --   T = {"<cmd>Telescope ToggleLSP<cr>", "LSP" },
+  --   w = { "<cmd>Telescope diagnostics<cr>", "Workspace Diagnostics", },
+  --   ["t"] = {
+  --     name = "+trouble",
+  --     t = { "<cmd>TroubleToggle<cr>", "trouble" },
+  --     w = { "<cmd>TroubleToggle workspace_diagnostics<cr>", "workspace" },
+  --     d = { "<cmd>TroubleToggle document_diagnostics<cr>", "document" },
+  --     q = { "<cmd>TroubleToggle quickfix<cr>", "quickfix" },
+  --     l = { "<cmd>TroubleToggle loclist<cr>", "loclist" },
+  --     r = { "<cmd>TroubleToggle lsp_references<cr>", "references" },
+  --   },
+  -- },
+  --
+ -------------------------------------------------------------------------------- 
+  -- M -- 
+ -------------------------------------------------------------------------------- 
+
+ -------------------------------------------------------------------------------- 
+  -- N -- 
+ -------------------------------------------------------------------------------- 
+  --
+  -- ["n"] = {
+  --   name = "Notes/Neorg",
+  --   n = {"<cmd>Telescope neorg find_norg_files<cr>", "Find Notes"},
+  --   l = {"<cmd>Telescope neorg find_linkable<cr>", "Linkable Notes"},
+  --   s = {"<cmd>Telescope neorg find_linkable<cr>", "Search Headings"},
+  --   N = {"<cmd>Neorg keybind norg core.norg.dirman.new.note<cr>", "New Note"},
+  --   w = {"<cmd>Telescope neorg switch_workspace<cr>", "Switch Workspace"},
+  --   r = {"<cmd>Neorg return<cr>", "Neorg Return"},
+  --   j = {
+  --     name = "journal",
+  --     j = {"<cmd>Neorg journal today<cr>", "Today's Journal"},
+  --     t = {"<cmd>Neorg journal tomorrow<cr>", "tomorrow's Journal"},
+  --     y = {"<cmd>Neorg journal yesterday<cr>", "Yesterday's Journal"},
+  --   },
+  -- },
+
+ -------------------------------------------------------------------------------- 
+  -- O -- 
+ -------------------------------------------------------------------------------- 
+
+  ["o"] = {
+    name = "+open",
+    m = {
+      name = "+my",
+      ["0"] = { "<cmd>edit $HOME/Nextcloud/Notes/zk/0INBOX.md<cr>",     "Inbox file" },
+      q = { "<cmd>edit $HOME/Nextcloud/Notes/zk/QuickNote.md<cr>",      "Inbox file" },
+      j = { "<cmd>edit $HOME/Nextcloud/Notes/zk/joyent/index.md<cr>",   "Open My Task" },
+      G = { "<cmd>edit $HOME/Nextcloud/Notes/zk/joyent/gage/index.md",  "Open Gage" },
+    },
+  },
+
+ -------------------------------------------------------------------------------- 
+  -- P -- 
+ -------------------------------------------------------------------------------- 
+
+  -- ["p"] = { 
+  --   name = "+project",
+  --   p = { "<cmd>lua require('telescope').extensions.projects.projects()<cr>", "Projects" },
+  --   s = { "<cmd>Telescope live_grep<cr>", "Projects" },
+  -- },
+
+ -------------------------------------------------------------------------------- 
+  -- Q -- 
+ -------------------------------------------------------------------------------- 
+
+  ["q"] = { "<cmd>q!<CR>", "Quit" },
+  --
+ -------------------------------------------------------------------------------- 
+  -- R -- 
+ -------------------------------------------------------------------------------- 
+
+ -------------------------------------------------------------------------------- 
+  -- S -- 
+ -------------------------------------------------------------------------------- 
+
+  ["s"] = {
+    name = "+search",
+    b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
+    c = { "<cmd>Telescope colorscheme<cr>", "Colorscheme" },
+    h = { "<cmd>Telescope help_tags<cr>", "Find Help" },
+    M = { "<cmd>Telescope man_pages<cr>", "Man Pages" },
+    r = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+    R = { "<cmd>Telescope registers<cr>", "Registers" },
+    k = { "<cmd>Telescope keymaps<cr>", "Keymaps" },
+  },
+
+ -------------------------------------------------------------------------------- 
+  -- T -- 
+ -------------------------------------------------------------------------------- 
+
+  ["T"] = {
+    name = "+telescope",
+    a = {"<cmd>Telescope autocommands<cr>", "autoommands" },
+    c = {
+      name = "+command",
+      c = {"<cmd>Telescope commands<cr>", "Commands" },
+      h = {"<cmd>Telescope command_history<cr>", "Command History" },
+    },
+    e = {"<cmd>Telescope env<cr>", "Environment" },
+    f = {"<cmd>Telescope find_pickers<cr>", "list pickers" },
+    F = {"<cmd>Telescope filetypes<cr>", "filetypes" },
+    g = {"<cmd>Telescope glyph<cr>", "Glyph" },
+    l = {"<cmd>Telescope luasnip<cr>", "luasnip" },
+    m = {"<cmd>Telescope marks<cr>", "Marks" },
+    M = {"<cmd>Telescope Man_pages<cr>", "Man Pages" },
+    p = {"<cmd>Telescope packer<cr>", "Packer" },
+    t = {"<cmd>Telescope<cr>", "Telescope" },
+    v = {"<cmd>Telescope vim_options<cr>", "Vim Options" },
+    z = {"<cmd>Telescope zoxide list<cr>", "Zoxide" },
+  },
+
+ -------------------------------------------------------------------------------- 
+  -- Z -- 
+ -------------------------------------------------------------------------------- 
+
+}
+
+wk.register(mappings, wkopts)
