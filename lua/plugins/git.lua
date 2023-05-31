757c99b08cb361d2fd9af41f4d5255203cf5a227
Marty Buchaus <marty@dabuke.com>
2023-05-31 08:49:19 -0400

    Update config of lazygit to add telescope

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -31,7 +31,11 @@ return {
     -- optional for floating window border decoration
     dependencies = {
       "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
     },
+    config = function ()
+      require("telescope").load_extension("lazygit")
+    end,
   },
 
 
