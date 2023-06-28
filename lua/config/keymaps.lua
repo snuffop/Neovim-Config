30d9ff90d32e422bed9faa4f3b6fdc29534f447e
Marty Buchaus <marty@dabuke.com>
2023-06-27 22:23:53 -0400

    Update Mutt config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -21,6 +21,18 @@ local wkopts = {
 --   visual_block_mode = "x",
 --   term_mode = "t",
 --   command_mode = "c",
+        local nvim_tmux_nav = require('nvim-tmux-navigation')
+
+        nvim_tmux_nav.setup {
+            disable_when_zoomed = true -- defaults to false
+        }
+vim.keymap.set('n', "<C-h>", nvim_tmux_nav.NvimTmuxNavigateLeft)
+vim.keymap.set('n', "<C-j>", nvim_tmux_nav.NvimTmuxNavigateDown)
+vim.keymap.set('n', "<C-k>", nvim_tmux_nav.NvimTmuxNavigateUp)
+vim.keymap.set('n', "<C-l>", nvim_tmux_nav.NvimTmuxNavigateRight)
+vim.keymap.set('n', "<C-\\>", nvim_tmux_nav.NvimTmuxNavigateLastActive)
+vim.keymap.set('n', "<C-Space>", nvim_tmux_nav.NvimTmuxNavigateNext)
+
 
 local mappings = {
 
