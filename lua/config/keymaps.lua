eedcc6208929012daae6c1c21fee4b92294f8863
Marty Buchaus <marty@dabuke.com>
2023-07-02 20:28:33 -0400

    all from archovo

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -16,46 +16,21 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("x", "<leader>p", [["_dP]])
-
 vim.keymap.set("n", "<leader>sr", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- dunno how to get working in whichkey
 
+-- TmuxNavigate 
 vim.keymap.set('n', "<C-h>", "<cmd>TmuxNavigateLeft<cr>")
 vim.keymap.set('n', "<C-j>", "<cmd>TmuxNavigateDown<cr>")
 vim.keymap.set('n', "<C-k>", "<cmd>TmuxNavigateUp<cr>")
 vim.keymap.set('n', "<C-l>", "<cmd>TmuxNavigateRight<cr>")
 
--- vim.keymap.set("n", "J", "mzJ`z")
--- vim.keymap.set("n", "<C-d>", "<C-d>zz")
--- vim.keymap.set("n", "<C-u>", "<C-u>zz")
--- vim.keymap.set("n", "n", "nzzzv")
--- vim.keymap.set("n", "N", "Nzzzv")
---
--- vim.keymap.set("n", "<leader>vwm", function()
---     require("vim-with-me").StartVimWithMe()
--- end)
--- vim.keymap.set("n", "<leader>svwm", function()
---     require("vim-with-me").StopVimWithMe()
--- end)
---
--- -- greatest remap ever
---
--- -- next greatest remap ever : asbjornHaland
--- vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
--- vim.keymap.set("n", "<leader>Y", [["+Y]])
---
--- vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
---
--- -- This is going to get me cancelled
--- vim.keymap.set("i", "<C-c>", "<Esc>")
---
--- vim.keymap.set("n", "Q", "<nop>")
--- vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
--- vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
---
--- vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
--- vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
--- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
--- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+vim.keymap.set("n", "i", function()   -- indent properly after i
+  if #vim.fn.getline "." == 0 then
+    return [["_cc]]
+  else
+    return "i"
+  end
+end, { expr = true })
 
 -- Whichkey additions 
 
@@ -147,8 +122,8 @@ local mappings = {
       l = { "<cmd>ObsidianLink<cr>", "Link" },
       n = { "<cmd>ObsidianNew<cr>", "New Note" },
       o = { "<cmd>ObsidianOpen<cr>", "Open" },
-      q = { "<cmd>ObsidianQuickSwitch<cr>", "Quick Switch" },
-      s = { "<cmd>ObsidianSearch<cr>", "Search" },
+      s = { "<cmd>ObsidianQuickSwitch<cr>", "Quick Switch" },
+      g = { "<cmd>ObsidianSearch<cr>", "Search" },
       t = { "<cmd>ObsidianToday<cr>", "Today" },
       y = { "<cmd>ObsidianYesterday<cr>", "Yesterday" },
     },
@@ -180,3 +155,6 @@ local mappings = {
 }
 
 wk.register(mappings, wkopts)
+
+
+
