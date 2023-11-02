eeed3f0446ec8a38f550528ef85ae42abf440613
Marty Buchaus <marty@dabuke.com>
2023-11-02 15:22:12 -0400

    Clean up breaking changes to Neogit

--- lua/plugins/neogit.lua
+++ lua/plugins/neogit.lua
@@ -1,23 +1,14 @@
 -- Neogit
-local installed, NeoGit = pcall(require, "neogit")
+local installed, NeoGit = pcall(require, 'neogit')
 if not installed then
     vim.notify("Plugin 'NeoGit' is not installed")
     return
 end
 
-NeoGit.setup({
-    commit_confirmation = {
-        enabled = true,
-        close_on_deny = false,
-    },
-    mappings = {
-        status = {
-            ["p"] = "PushPopup",
-            ["F"] = "PullPopup",
-        },
-    },
+NeoGit.setup {
     integrations = {
         diffview = true,
         telescope = true,
+        fzf_lua = true,
     },
-})
+}
