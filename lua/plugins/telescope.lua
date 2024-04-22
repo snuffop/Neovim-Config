a5cb4a9d3a093737d4d1713b81d9ee980671afd3
Marty Buchaus <marty@dabuke.com>
2024-04-22 09:57:49 -0400

    add telescope config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,59 +1,65 @@
 -- Telescope config
 
 return {
-
-  "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "debugloop/telescope-undo.nvim",
+  {
     "nvim-telescope/telescope-fzf-native.nvim",
-    "jvgrootveld/telescope-zoxide",
-    build = "make",
-    config = function()
-      require("telescope").load_extension("fzf")
-      require("telescope").load_extension("undo")
-      require("telescope").load_extension("zoxide")
-    end,
+    build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
   },
+  {
 
-  keys = {
-        -- add a keymap to browse plugin files
-        -- stylua: ignore
-        {
-            "<leader>fp",
-            function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
-            desc = "Find Plugin File",
-        },
-  },
+    "nvim-telescope/telescope.nvim",
+    dependencies = {
+      "debugloop/telescope-undo.nvim",
+      "nvim-telescope/telescope-fzf-native.nvim",
+      "jvgrootveld/telescope-zoxide",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+        require("telescope").load_extension("undo")
+        require("telescope").load_extension("zoxide")
+      end,
+    },
 
-  opts = {
-    defaults = {
-      layout_strategy = "horizontal",
-      layout_config = { prompt_position = "top" },
-      sorting_strategy = "ascending",
-      winblend = 0,
-      mappings = {
-        i = {
-          ["<C-j>"] = "move_selection_next",
-          ["<C-k>"] = "move_selection_previous",
+    keys = {
+            -- add a keymap to browse plugin files
+            -- stylua: ignore
+            {
+                "<leader>fp",
+                function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+                desc = "Find Plugin File",
+            },
+    },
+
+    opts = {
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = { prompt_position = "top" },
+        sorting_strategy = "ascending",
+        winblend = 0,
+        mappings = {
+          i = {
+            ["<C-j>"] = "move_selection_next",
+            ["<C-k>"] = "move_selection_previous",
+          },
         },
       },
-    },
-    extensions = {
-      zoxide = {
-        prompt_title = "[ Walking on the shoulders of TJ ]",
-        mappings = {
-          default = {
-            after_action = function(selection)
-              print("Update to (" .. selection.z_score .. ") " .. selection.path)
-            end,
-          },
-          ["<C-s>"] = {
-            before_action = function(selection)
-              print("before C-s")
-            end,
-            action = function(selection)
-              vim.cmd.edit(selection.path)
-            end,
+      extensions = {
+        zoxide = {
+          prompt_title = "[ Walking on the shoulders of TJ ]",
+          mappings = {
+            default = {
+              after_action = function(selection)
+                print("Update to (" .. selection.z_score .. ") " .. selection.path)
+              end,
+            },
+            ["<C-s>"] = {
+              before_action = function(selection)
+                print("before C-s")
+              end,
+              action = function(selection)
+                vim.cmd.edit(selection.path)
+              end,
+            },
           },
         },
       },
