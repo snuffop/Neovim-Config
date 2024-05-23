f353ba9e272a30276725523612b55d56caf1d87c
Marty Buchaus <marty@dabuke.com>
2024-05-23 08:01:29 -0400

    add mason-bridge

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -9,16 +9,4 @@ return {
     "nvimdev/dashboard-nvim",
     enable = false,
   },
-  {
-    "mfussenegger/nvim-lint",
-    config = function()
-      local markdownlint = require("lint").linters.markdownlint
-      markdownlint.args = {
-        "--disable",
-        "MD013",
-        "MD007",
-        "--", -- Required
-      }
-    end,
-  },
 }
--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -1,12 +1,58 @@
 -- add any tools you want to have installed below
+-- https://github.com/frostplexx/mason-bridge.nvim
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    ensure_installed = {
-      "stylua",
-      "shellcheck",
-      "shfmt",
-      "flake8",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "stylua",
+        "shellcheck",
+        "shfmt",
+        "flake8",
+      },
     },
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "frostplexx/mason-bridge.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+    },
+    config = function()
+      require("mason-bridge").setup()
+    end,
+  },
+
+  {
+    "stevearc/conform.nvim",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "frostplexx/mason-bridge.nvim",
+    },
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = require("mason-bridge").get_formatters(),
+      })
+    end,
+  },
+
+  {
+    "mfussenegger/nvim-lint",
+    depnedencies = {
+      "williamboman/mason.nvim",
+      "frostplexx/mason-bridge.nvim",
+    },
+    config = function()
+      local markdownlint = require("lint").linters.markdownlint
+      markdownlint.args = {
+        "--disable",
+        "MD013",
+        "MD007",
+        "--", -- Required
+      }
+    end,
   },
 }
