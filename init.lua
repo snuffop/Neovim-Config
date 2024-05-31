ee963b55ed57dc5a8250cd1114b598a016cd4b50
Marty Buchaus <marty@dabuke.com>
2024-05-31 18:23:36 -0400

    Clean

--- init.lua
+++ init.lua
@@ -2,13 +2,5 @@
 --  │ Marty Buchaus Neovim Config                              │
 --  ╰──────────────────────────────────────────────────────────╯
 
-function get_hostname()
-  local f = io.popen("/bin/hostname")
-  local hostname = f:read("a") or ""
-  f:close()
-  hostname = string.gsub(hostname, "\n$", "")
-  return hostname
-end
-
 require("config/general")
 require("config/lazy")
--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -1,7 +1,6 @@
 return {
   {
     "folke/tokyonight.nvim",
-    lazy = true,
-    enable = false,
+    lazy = false,
   },
 }
