3c6ce53914f2be444a60a4c6545dbe3b67df310c
Marty Buchaus <marty@dabuke.com>
2024-07-01 10:10:03 -0400

    Move lazy json

--- init.lua
+++ init.lua
@@ -2,5 +2,7 @@
 --  │ Marty Buchaus Neovim Config                              │
 --  ╰──────────────────────────────────────────────────────────╯
 
+-- lua/init.luajsdf
+vim.g.lazyvim_json = vim.fn.stdpath("config") .. "/etc/lazyvim.json"
 require("config/general")
 require("config/lazy")
