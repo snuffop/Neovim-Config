5758d2969e51516ee50edb89cae765c586b58d04
Marty Buchaus <marty@dabuke.com>
2024-07-09 11:41:07 -0400

    add neorg completions

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -4,7 +4,16 @@ return {
   "hrsh7th/nvim-cmp",
   ---@param opts cmp.ConfigSchema
   opts = function(_, opts)
+    table.insert(opts.sources, { name = "neorg" })
+
     local cmp = require("cmp")
+
+    local has_words_before = function()
+      unpack = unpack or table.unpack
+      local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+      return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+    end
+
     opts.preselect = cmp.PreselectMode.None
     opts.completion = { completeopt = "menu,menuone,preview,noinsert,noselect" }
     opts.experimental.ghost_text = false
