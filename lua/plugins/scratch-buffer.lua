dc748743466f5cd440323887f73158501078844c
Marty Buchaus <marty@dabuke.com>
2024-09-26 09:52:37 -0600

    all

---
+++ lua/plugins/scratch-buffer.lua
@@ -0,0 +1,9 @@
+
+return {
+    lazy = false,
+    dir = "~/Source/ghq/github.com/snuffop/scratch-buffer",
+    name = "scratch-buffer",
+    config = function ()
+        require('scratch-buffer').setup()
+    end
+}
