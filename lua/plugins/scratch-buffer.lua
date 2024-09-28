59b1b93cc086a03e4f5d874ccbdcc9a8b9d737d6
William Marty Buchaus <marty@dabuke.com>
2024-09-28 16:44:49 -0400

    changes from archovo on Sat Sep 28 04:44:49 PM EDT 2024

--- lua/plugins/scratch-buffer.lua
+++ lua/plugins/scratch-buffer.lua
@@ -1,9 +1,9 @@
 
 return {
-    lazy = false,
-    dir = "~/Source/ghq/github.com/snuffop/scratch-buffer",
-    name = "scratch-buffer",
-    config = function ()
-        require('scratch-buffer').setup()
-    end
+    -- lazy = false,
+    -- dir = "~/Source/ghq/github.com/snuffop/scratch-buffer",
+    -- name = "scratch-buffer",
+    -- config = function ()
+    --     require('scratch-buffer').setup()
+    -- end
 }
