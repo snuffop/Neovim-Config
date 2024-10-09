491faf93c234f5a2329b2d245119d8e64c7d2103
Marty Buchaus <marty@dabuke.com>
2024-10-09 12:22:13 -0400

    correct Harper config

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -1,6 +1,6 @@
---  ╭──────────────────────────────────────────────────────────╮
---  │ Marty Buchaus Neovim Configuration                       │
---  ╰──────────────────────────────────────────────────────────╯
+--  ╭────────────────────────────────────╮
+--  │ Marty Buchaus Neovim Configuration │
+--  ╰────────────────────────────────────╯
 
 -- add tsserver and setup with typescript.nvim instead of lspconfig
 --local lapi = os.execute("gopass show -o Application/language-tool")
@@ -24,43 +24,71 @@ return {
             -- tsserver will be automatically installed with mason and loaded with lspconfig
             tsserver = {},
             pyright = {},
-            harper_ls = {},
+            lua_ls = {
+                settings = {
+                    Lua = {
+                        workspace = {
+                            checkThirdParty = false,
+                        },
+                        codeLens = {
+                            enable = true,
+                        },
+                        completion = {
+                            callSnippet = "Replace",
+                        },
+                        doc = {
+                            privateName = { "^_" },
+                        },
+                        hint = {
+                            enable = true,
+                            setType = false,
+                            paramType = true,
+                            paramName = "Disable",
+                            semicolon = "Disable",
+                            arrayIndex = "Disable",
+                        },
+                    },
+                },
+            },
+            harper_ls = {
+                settings = {
+                    ["harper-ls"] = {
+                        userDictPath = "~/.config/nvim/spell/harper-dict.txt",
+                        diagnosticSeverity = "hint", -- Can also be "information", "warning", or "error"
+                        linters = {
+                            spell_check = true,
+                            spelled_numbers = false,
+                            an_a = true,
+                            sentence_capitalization = true,
+                            unclosed_quotes = true,
+                            wrong_quotes = false,
+                            long_sentences = true,
+                            repeated_words = true,
+                            spaces = true,
+                            matcher = true,
+                            correct_number_suffix = true,
+                            number_suffix_capitalization = true,
+                            multiple_sequential_pronouns = true,
+                            linking_verbs = false,
+                            avoid_curses = true,
+                            terminating_conjunctions = true
+                        },
+                    },
+                },
+            },
         },
         -- you can do any additional lsp server setup here
         -- return true if you don't want this server to be setup with lspconfig
         ---@type table<string, fun(server:string, opts:_.lspconfig.options):boolean?>
         setup = {
+            autoformat = false,
             -- example to setup with typescript.nvim
             tsserver = function(_, opts)
                 require("typescript").setup({ server = opts })
                 return true
             end,
-            autoformat = false,
-            settings = {
-                ["harper-ls"] = {
-                    wrong_quotes = true,
-                    userDictPath = "~/.config/nvim/spell/harper-dict.txt",
-                    diagnosticSeverity = "hint", -- Can also be "information", "warning", or "error"
-                    linters = {
-                        spell_check = true,
-                        spelled_numbers = false,
-                        an_a = true,
-                        sentence_capitalization = true,
-                        unclosed_quotes = true,
-                        wrong_quotes = false,
-                        long_sentences = true,
-                        repeated_words = true,
-                        spaces = true,
-                        matcher = true,
-                        correct_number_suffix = true,
-                        number_suffix_capitalization = true,
-                        multiple_sequential_pronouns = true,
-                        linking_verbs = false,
-                        avoid_curses = true,
-                        terminating_conjunctions = true
-                    }
-                }
-            },
-        },
-    },
+        }
+    }
 }
+
+
