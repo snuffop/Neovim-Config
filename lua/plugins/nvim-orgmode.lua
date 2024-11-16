77aa60a41b2f6ce25d87f3c506edd5a0b92738b8
William Marty Buchaus <marty@dabuke.com>
2024-11-16 16:07:15 -0500

    changes from archovo on Sat Nov 16 04:07:15 PM EST 2024

--- lua/plugins/nvim-orgmode.lua
+++ lua/plugins/nvim-orgmode.lua
@@ -28,5 +28,21 @@ return{
         config = function()
             require('org-bullets').setup()
         end
+    },
+
+    {
+        "nvim-orgmode/telescope-orgmode.nvim",
+        event = "VeryLazy",
+        dependencies = {
+            "nvim-orgmode/orgmode",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = function()
+            require("telescope").load_extension("orgmode")
+
+            vim.keymap.set("n", "<leader>oR", require("telescope").extensions.orgmode.refile_heading)
+            vim.keymap.set("n", "<leader>ofh", require("telescope").extensions.orgmode.search_headings)
+            vim.keymap.set("n", "<leader>oli", require("telescope").extensions.orgmode.insert_link)
+        end,
     }
 }
