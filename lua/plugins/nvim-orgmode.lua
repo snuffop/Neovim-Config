9e69cff6cb0d4fa98fb88652f0b3d425777a37f4
William Marty Buchaus <marty@dabuke.com>
2024-11-16 15:35:16 -0500

    changes from archovo on Sat Nov 16 03:35:16 PM EST 2024

--- lua/plugins/nvim-orgmode.lua
+++ lua/plugins/nvim-orgmode.lua
@@ -3,15 +3,21 @@
 --  ╰──────────────────────────────────────────────────────────╯
 
 return{
-    -- 'nvim-orgmode/orgmode',
-    -- event = 'VeryLazy',
-    -- ft = { 'org' },
-    -- config = function()
-    --     -- Setup orgmode
-    --     require('orgmode').setup({
-    --         org_agenda_files = '~/Nextcloud/Notes/org/orgzly/**/*',
-    --         org_default_notes_file = '~/Nextcloud/Notes/org/orgzly/inbox.org',
-    --     })
-    --
-    -- end,
+    'nvim-orgmode/orgmode',
+    event = 'VeryLazy',
+    ft = { 'org' },
+    config = function()
+        -- Setup orgmode
+        require('orgmode').setup({
+            org_agenda_files = '~/orgfiles/**/*',
+            org_default_notes_file = '~/orgfiles/refile.org',
+        })
+
+        -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+        -- add ~org~ to ignore_install
+        require('nvim-treesitter.configs').setup({
+          ensure_installed = 'all',
+          ignore_install = { 'org' },
+        })
+    end,
 }
