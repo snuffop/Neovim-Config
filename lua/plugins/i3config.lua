7669263cc74a3a915b61d56631bfa75ccd6c44ee
Marty Buchaus <marty@dabuke.com>
2024-12-06 14:01:18 -0500

    changes from archzen on Fri Dec 6 02:01:18 PM EST 2024

--- lua/plugins/i3config.lua
+++ lua/plugins/i3config.lua
@@ -10,8 +10,12 @@ return {
     vim.cmd([[
         aug i3config_ft_detection
         au!
-        au BufNewFile,BufRead ~/.config/i3/config set filetype=i3config
-        au BufNewFile,BufRead ~/.config/sway/config set filetype=i3config
+        au BufNewFile,BufRead ~/.config/i3/config call SetI3Config()
+        au BufNewFile,BufRead ~/.config/sway/config call SetI3Config()
+
+        function SetI3Config()
+            setlocal commentstring=#
+            set filetype=i3config
         aug end
     ]])
   end,
