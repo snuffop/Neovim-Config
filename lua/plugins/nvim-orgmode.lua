d8ed7b27572488ae421c0489e3de6a390b35fa44
Marty Buchaus <marty@dabuke.com>
2025-01-02 08:46:47 -0500

    changes from archzen on Thu Jan 2 08:46:47 AM EST 2025

--- lua/plugins/nvim-orgmode.lua
+++ lua/plugins/nvim-orgmode.lua
@@ -10,8 +10,8 @@ return{
         config = function()
             -- Setup orgmode
             require('orgmode').setup({
-                org_agenda_files = '~/orgfiles/**/*',
-                org_default_notes_file = '~/orgfiles/refile.org',
+                rg_agenda_files = "~/Nextcloud/Org/**/*",
+                org_default_notes_file = "~/Nextcloud/Org/Orgzly/Inbox.org",
             })
             -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
             -- add ~org~ to ignore_install
@@ -24,24 +24,9 @@ return{
 
     {
         'akinsho/org-bullets.nvim', 
+        ft = { "org" },
         config = function()
             require('org-bullets').setup()
         end
     },
-
-    -- {
-    --     "nvim-orgmode/telescope-orgmode.nvim",
-    --     event = "VeryLazy",
-    --     dependencies = {
-    --         "nvim-orgmode/orgmode",
-    --         "nvim-telescope/telescope.nvim",
-    --     },
-    --     config = function()
-    --         require("telescope").load_extension("orgmode")
-    --
-    --         vim.keymap.set("n", "<leader>oR", require("telescope").extensions.orgmode.refile_heading)
-    --         vim.keymap.set("n", "<leader>ofh", require("telescope").extensions.orgmode.search_headings)
-    --         vim.keymap.set("n", "<leader>oli", require("telescope").extensions.orgmode.insert_link)
-    --     end,
-    -- }
 }
