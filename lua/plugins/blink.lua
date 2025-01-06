246d9c4a770d2a3ce61cac844e97fad27920b478
Marty Buchaus <marty@dabuke.com>
2025-01-06 11:41:06 -0500

    changes from archzen on Mon Jan 6 11:41:06 AM EST 2025

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -2,15 +2,16 @@
 --  │ Marty Buchaus Neovim Configuration                       │
 --  ╰──────────────────────────────────────────────────────────╯
 
+-- BLINK Obsidian  Config
 return {
+
     "saghen/blink.cmp",
+    dependencies = { 
+        { "epwalsh/obsidian.nvim", "saghen/blink.compat" },
+    },
     opts = {
         sources = {
-            compat = {
-                "obsidian",
-                "obsidian_new",
-                "obsidian_tags",
-            }
-        }
-    }
+            compat = { "obsidian", "obsidian_new", "obsidian_tags" },
+        },
+    },
 }
--- lua/plugins/obsidian.lua
+++ lua/plugins/obsidian.lua
@@ -129,28 +129,4 @@ return {
         },
     },
 
-    -- BLINK Obsidian  Config
-    {
-        "saghen/blink.cmp",
-        dependencies = { "saghen/blink.compat" },
-        opts = {
-            sources = {
-                default = { "obsidian", "obsidian_new", "obsidian_tags" },
-                providers = {
-                    obsidian = {
-                        name = "obsidian",
-                        module = "blink.compat.source",
-                    },
-                    obsidian_new = {
-                        name = "obsidian_new",
-                        module = "blink.compat.source",
-                    },
-                    obsidian_tags = {
-                        name = "obsidian_tags",
-                        module = "blink.compat.source",
-                    },
-                },
-            },
-        },
-    }
 }
