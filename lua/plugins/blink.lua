1a7fc5296ee4850ad947ece4d5c6eb96a9cdb724
Marty Buchaus <marty@dabuke.com>
2025-01-08 15:35:31 -0500

    changes from archzen on Wed Jan 8 03:35:31 PM EST 2025

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -7,52 +7,16 @@ return {
     dependencies = { 
         "epwalsh/obsidian.nvim",
         "saghen/blink.compat",
-        "mikavilpas/blink-ripgrep.nvim",
     },
     opts = {
         sources = {
             compat = { "obsidian", "obsidian_new", "obsidian_tags" },
-            default = {
-                "ripgrep",
-            },
-            providers = {
-                ripgrep = {
-                    module = "blink-ripgrep",
-                    name = "Ripgrep",
-                    ---@module "blink-ripgrep"
-                    ---@type blink-ripgrep.Options
-                    opts = {
-                        prefix_min_len = 3,
-                        context_size = 5,
-                        max_filesize = "1M",
-                        project_root_marker = ".git",
-                        search_casing = "--ignore-case",
-                        additional_rg_options = {},
-                        fallback_to_regex_highlighting = true,
-                        debug = false,
-                    },
-                    transform_items = function(_, items)
-                        for _, item in ipairs(items) do
-                            item.labelDetails = {
-                                description = "(rg)",
-                            }
-                        end
-                        return items
-                    end,
-                },
-            },
         },
     },
     keymap = {
         preset = "enter",
         ["<Tab>"] = { "select_next", "snippet_forward", "fallback" },
         ["<S-Tab>"] = { "select_prev", "snippet_backward", "fallback" },
-        ["<c-g>"] = {
-            function()
-                -- invoke manually, requires blink >v0.8.0
-                require("blink-cmp").show({ providers = { "ripgrep" } })
-            end,
-        },
     },
     completion = {
         list = {
