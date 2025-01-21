2508782ef60477e27693ecb867f9740f395aab88
Marty Buchaus <marty@dabuke.com>
2025-01-21 12:11:32 -0500

    changes from archzen on Tue Jan 21 12:11:32 EST 2025

--- lua/plugins/nvim-orgmode.lua
+++ lua/plugins/nvim-orgmode.lua
@@ -12,10 +12,23 @@ return{
             require('orgmode').setup({
                 org_agenda_files = "~/Nextcloud/Org/Orgzly/*",
                 org_default_notes_file = "~/Nextcloud/Org/Orgzly/Inbox.org",
-                org_todo_keywords 
+                org_todo_keywords = {'TODO(t)', 'STARTED(s)', 'WAITING(w)', 'DELEGATED', '|', 'ASSIGNED', 'CANCELLED(c)', 'DONE(d)' },
+                org_todo_keyword_faces = {
+                    TODO = ':foreground "red"     :weight bold',
+                    STARTED    = ':foreground "#E35DBF" :weight bold',
+                    WAITING    = ':foreground "#008080" :weight bold',
+                    WAIT       = ':foreground "#008080" :weight bold',
+                    DELEGATED  = ':foreground "pink"    :weight bold',
+                    CANCELLED  = ':foreground "white"   :weight bold',
+                    ASSIGNED   = ':foreground "white"   :weight bold',
+                    DONE       = ':foreground "green"   :weight bold',
+                },
+                org_log_into_drawer = "LOGBOOK",
+                emacs_config = "~/.config/doom/init.el",
             })
-            -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
-            -- add ~org~ to ignore_install
+
+                -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+                -- add ~org~ to ignore_install
             require('nvim-treesitter.configs').setup({
                 ensure_installed = 'all',
                 ignore_install = { 'org' },
