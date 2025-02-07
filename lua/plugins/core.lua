04789dc776da9bbdd9caf8bf3d7afc2ba979b444
William Marty Buchaus <marty@dabuke.com>
2025-02-06 19:42:58 -0500

    changes from archovo on Thu Feb 6 07:42:58 PM EST 2025

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -91,19 +91,5 @@ return {
         lazy = false,
     },
 
-    vim.api.nvim_create_autocmd('FileType', {
-        pattern = 'markdown',
-        callback = function(args)
-            vim.lsp.start({
-                name = 'iwes',
-                cmd = {'iwes'},
-                root_dir = vim.fs.root(args.buf, {'.iwe' }),
-                flags = {
-                    debounce_text_changes = 500
-                }
-            })
-        end,
-    })
-
 }
 
