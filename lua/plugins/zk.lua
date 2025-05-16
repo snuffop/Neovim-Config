e3001110dda583c06bf05b737ede11fe3eaf76bf
Marty Buchaus <marty@dabuke.com>
2025-05-16 07:56:47 -0400

    changes from archzen on Fri May 16 07:56:47 EDT 2025

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -5,29 +5,19 @@
 -- ZK config
 
 return {
-    -- "zk-org/zk-nvim",
-    -- lazy = false,
-    -- branch = 'fix/client-nil-error',
-    -- config = function ()
-    --     require("zk").setup({
-    --         picker = "snacks_picker",
-    --         picker_options = {
-    --             snacks_picker = {
-    --                 layout = {
-    --                     preset = "ivy",
-    --                 },
-    --             },
-    --         },
-    --         lsp = {
-    --             config = {
-    --                 cmd = {"zk", "lsp"},
-    --                 name = "zk",
-    --             }
-    --         },
-    --         auto_attach = {
-    --             enable = true,
-    --             filetypes = { "markdown" },
-    --         }
-    --     })
-    -- end
+    "zk-org/zk-nvim",
+    lazy = false,
+    branch = 'fix/client-nil-error',
+    config = function ()
+        require("zk").setup({
+            picker = "snacks_picker",
+            -- picker_options = {
+            --     snacks_picker = {
+            --         layout = {
+            --             preset = "ivy",
+            --         },
+            --     },
+            -- },
+        })
+    end
 }
