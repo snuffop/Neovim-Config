922ead38dd8148dd351bc3f9c203fd2fa4c3058f
Marty Buchaus <marty@dabuke.com>
2025-05-16 08:11:45 -0400

    changes from archzen on Fri May 16 08:11:45 EDT 2025

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -11,13 +11,23 @@ return {
     config = function ()
         require("zk").setup({
             picker = "snacks_picker",
-            -- picker_options = {
-            --     snacks_picker = {
-            --         layout = {
-            --             preset = "ivy",
-            --         },
-            --     },
-            -- },
+            picker_options = {
+                snacks_picker = {
+                    layout = {
+                        preset = "ivy",
+                    },
+                },
+            },
+            lsp = {
+                config = {
+                    cmd = {"zk", "lsp"},
+                    name = "zk",
+                }
+            },
+            auto_attach = {
+                enable = true,
+                filetypes = { "markdown" },
+            }
         })
     end
 }
