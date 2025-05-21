0f3626eb18a45c8279c48bc5cb3e404179b09906
Marty Buchaus <marty@dabuke.com>
2025-05-21 16:28:10 -0400

    changes from archzen on Wed May 21 16:28:10 EDT 2025

--- lua/plugins/yamlmatter.lua
+++ lua/plugins/yamlmatter.lua
@@ -3,24 +3,40 @@
 --  ╰────────────────────────────────────╯
 
 return {
-    "ray-x/yamlmatter.nvim",
-    ft = { "markdown" },
-    config = function()
-        require('yamlmatter').setup({
-            icon_mappings = {
-                title = '',
-                idea = '',
-                default = '󰦨',
-            },
-            highlight_groups = {
-                icon = 'MyIconHighlight',
-                key = 'MyKeyHighlight',
-                value = 'MyValueHighlight',
-            },
-            key_value_padding = 4, -- Less space
-            conceallevel = 2, -- on what level start conceal the yaml text
+    {
+        "cuducos/yaml.nvim",
+        ft = { 
+            "yaml",
+            "yml",
+        }, -- optional
+        dependencies = {
+            "nvim-treesitter/nvim-treesitter",
+            "folke/snacks.nvim", -- optional
+            --"nvim-telescope/telescope.nvim", -- optional
+            --"ibhagwan/fzf-lua" -- optional
+        },
+    },
 
-        })
-    end
+    {
+        "ray-x/yamlmatter.nvim",
+        ft = { "markdown" },
+        config = function()
+            require('yamlmatter').setup({
+                icon_mappings = {
+                    title = '',
+                    idea = '',
+                    default = '󰦨',
+                },
+                highlight_groups = {
+                    icon = 'MyIconHighlight',
+                    key = 'MyKeyHighlight',
+                    value = 'MyValueHighlight',
+                },
+                key_value_padding = 4, -- Less space
+                conceallevel = 2, -- on what level start conceal the yaml text
+
+            })
+        end
+    }
 }
 
