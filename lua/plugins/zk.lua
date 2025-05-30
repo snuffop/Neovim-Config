3e09d27ff069b89701fffd41a8579042d75e9444
Marty Buchaus <marty@dabuke.com>
2025-05-30 14:18:38 -0400

    changes from archzen on Fri May 30 14:18:38 EDT 2025

--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -29,19 +29,20 @@ return {
                     enable = true,
                 }
             })
-        end
-    },
 
-    local zk = require("zk")
-        local commands = require("zk.commands")
+            local zk = require("zk")
+            local commands = require("zk.commands")
             local function make_edit_fn(defaults, picker_options)
-            return function(options)
-                options = vim.tbl_extend("force", defaults, options or {})
-                zk.edit(options, picker_options)
+                return function(options)
+                    options = vim.tbl_extend("force", defaults, options or {})
+                    zk.edit(options, picker_options)
+                end
             end
-        end
 
             commands.add("ZkOrphans", make_edit_fn({ orphan = true }, { title = "Zk Orphans" }))
-        commands.add("ZkRecents", make_edit_fn({ createdAfter = "2 weeks ago" }, { title = "Zk Recents" }))
+            commands.add("ZkRecents", make_edit_fn({ createdAfter = "2 weeks ago" }, { title = "Zk Recents" }))
 
+        end
+    },
 }
+
