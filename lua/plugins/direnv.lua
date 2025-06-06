6b7b54fde1633d18a29e74ef2b8b37bae268f75e
Marty Buchaus <marty@dabuke.com>
2025-06-06 10:42:33 -0400

    changes from archzen on Fri Jun 6 10:42:33 EDT 2025

--- lua/plugins/direnv.lua
+++ lua/plugins/direnv.lua
@@ -7,7 +7,15 @@
 --   https://github.com/direnv/direnv.vim?tab=readme-ov-file        --
 ----------------------------------------------------------------------
 
-return {
-    "direnv/direnv.vim",
-    lazy = false,
+-- return {
+--     "direnv/direnv.vim",
+--     lazy = false,
+-- }
+
+-- lets try  https://github.com/NotAShelf/direnv.nvim
+return{
+  "NotAShelf/direnv.nvim",
+  config = function()
+    require("direnv").setup({})
+  end,
 }
--- lua/plugins/faster.lua
+++ lua/plugins/faster.lua
@@ -2,6 +2,8 @@
 --  │ Marty Buchaus Neovim Configuration                       │
 --  ╰──────────────────────────────────────────────────────────╯
 
+-- https://github.com/pteroctopus/faster.nvim
+
 return {
     'pteroctopus/faster.nvim',
     lazy = false,
