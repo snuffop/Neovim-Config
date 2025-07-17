983f143c4d3db1cd3dd951a3b14571cabaabe861
Marty Buchaus <marty@dabuke.com>
2025-07-17 17:24:19 -0400

    limit wakatime as well

--- lua/plugins/wakatime.lua
+++ lua/plugins/wakatime.lua
@@ -2,7 +2,11 @@
 --  │ Marty Buchaus Neovim Configuration │
 --  ╰────────────────────────────────────╯
 
+if vim.uv.os_gethostname() == "archzen" or vim.uv.os_gethostname() == "archovo" then
 return {
     "wakatime/vim-wakatime",
     lazy = false,
 }
+else
+    return {}
+end
