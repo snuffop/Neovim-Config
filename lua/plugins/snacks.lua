55f172a3706eb9b7ad2c9dbf6c8e539881169310
Marty Buchaus <marty@dabuke.com>
2025-07-17 15:58:22 -0400

    Update last changes

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -11,18 +11,38 @@ return {
     lazy = false,
     opts = {
         bigfile = { enabled = true },
-        dashboard = { enabled = true },
         explorer = { enabled = true },
         image = { enabled = true },
         indent = { enabled = true },
         input = { enabled = true },
         notifier = { enabled = true },
-        picker = { enabled = true },
         quickfile = { enabled = true },
         scope = { enabled = true },
         scroll = { enabled = false },
         statuscolumn = { enabled = true },
         words = { enabled = true },
+        dashboard = { 
+            enabled = true,
+            preset = {
+                header = [[
+                                                          Z
+      █████                      ███     ██         z  
+     █████                        ███                   
+     ███   ███ ██████████████████ ██  █████ 
+    ███    ████     ██  ██████████ ███ ████████ 
+   ███    ████   ██    ███████████ ███ ███ ██ ███ 
+ █████████     ██ ██         ██ ████ ███ ███ ██ ███ 
+███████████████████████████████  ██ ███ ███ ██ ███]],
+            },
+        },
+        picker = { 
+            enabled = true,
+            matcher = {
+                cwd_bonus = true,
+                frecency = true,
+                history_bonus = true,
+            },
+        },
     },
-
 }
+
