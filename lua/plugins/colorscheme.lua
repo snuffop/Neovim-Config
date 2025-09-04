4406a53fe37b7d12039e869462d8d07932d3d656
Marty Buchaus <marty@dabuke.com>
2025-09-04 11:01:22 -0400

    Update Tokyonight colors for line numbers

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -4,35 +4,35 @@
 
 return {
 
+
     {
         "folke/tokyonight.nvim",
-        enable = true,
-        opts = { 
+        enabled = true,
+        priority = 1000,
+        opts = {
             style = "night",
             transparent = true,
+            on_highlights = function(hl, c)
+                hl.LineNr       = { fg = c.fg_dark }       -- softer than c.fg
+                hl.LineNrAbove  = { fg = c.fg_dark }
+                hl.LineNrBelow  = { fg = c.fg_dark }
+                hl.CursorLineNr = { fg = c.yellow, bold = true }
+            end,
         },
+        config = function(_, opts)
+            require("tokyonight").setup(opts)
+            vim.cmd.colorscheme("tokyonight")
+
+            vim.opt.number = true
+            vim.opt.relativenumber = true
+            vim.opt.signcolumn = "yes:1"
+        end,
     },
-
-
-    {
-        "catppuccin/nvim",
-        name = "catppuccin",
-        opts = {
-            transparent_backgroud = true,
-            flavour = "mocha",
-            integration = {
-                copilot = true,
-                blink_cmp = true,
-            }
-        }
-    },
-
 
     {
         "LazyVim/LazyVim",
         opts = {
             colorscheme = "tokyonight",
-            --colorscheme = "catppuccin",
             news = {
                 neovim = true,
             }
